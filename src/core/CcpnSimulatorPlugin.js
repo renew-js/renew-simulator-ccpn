75662b30eb08b4c0fd28085e15bc5a09fc60e827
Marc Richter <marc@richter.bz>
2019-07-01 22:59:32 +0200

    Add meta-model information

--- src/core/CcpnSimulatorPlugin.js
+++ src/core/CcpnSimulatorPlugin.js
@@ -3,14 +3,26 @@ class CcpnSimulatorPlugin {
     constructor () {
         this.name = 'CcpnSimulatorPlugin';
         this.provides = [
-            { id: 'ccpn', name: 'CCPN Formalism' },
+            {
+                id: 'ccpn',
+                name: 'CCPN Formalism',
+                metaModel: {
+                    type: 'pt',
+                    format: 'pnml-renew',
+                },
+            },
         ];
+        this.server = null;
     }
 
     initialize (server) {
         return new Promise((resolve, reject) => {
             resolve();
         });
+    }
+
+    start (formalismId, data) {
+        console.log(formalismId, data);
     }
 
 }
