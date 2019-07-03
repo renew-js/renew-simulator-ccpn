6f4f34d6429b729235a9ba8b1bc00be5a2787b83
Marc Richter <marc@richter.bz>
2019-07-04 00:15:42 +0200

    Add error and netMarking handlers

--- src/CCPNSimulatorPlugin.js
+++ src/CCPNSimulatorPlugin.js
@@ -1,7 +1,7 @@
-const builder = require('xmlbuilder');
+const xmlBuilder = require('xmlbuilder');
+const xmlParser = require('fast-xml-parser');
 
 const CCPNProcess = require('./CCPNProcess');
-
 
 class CCPNSimulatorPlugin {
 
@@ -17,31 +17,60 @@ class CCPNSimulatorPlugin {
                 },
             },
         ];
+        this.netInstance = null;
+        this.places = null;
+    }
 
+    initPlugin (serverManager) {
         this.ccpn = new CCPNProcess();
-        this.ccpn.read((data) => {
-            process.stdout.write(data);
+
+        serverManager.registerHandlers((socket) => {
+            this.ccpn.read((buffer) => {
+                const msg = buffer.toString().replace(/\n/g, '');
+                console.log(this.name + ': ', msg);
+
+                const data = xmlParser.parse(msg);
+                const elementName = Object.keys(data)[0];
+                switch(elementName) {
+                    case 'error':
+                        socket.emit('simulation.error', data[elementName]);
+                        break;
+                    case 'netMarking':
+                        this.updateMarking(socket, data[elementName]);
+                        break;
+                }
+            });
         });
     }
 
-    initSimulation (formalismId, data) {
-        console.log(formalismId, data);
-        const load = builder.create('load')
+    initSimulation (formalismId, netInstance, serializedData) {
+        this.netInstance = netInstance;
+        this.indexPlaces();
+        const load = xmlBuilder.create('load')
             // .att('baseFile', 'foo.pnml')
-            .raw(data);
+            .raw(serializedData.payload);
         this.ccpn.sendElement(load);
     }
 
+    indexPlaces () {
+        this.places = this.netInstance.elements.filter((element) => {
+            return element.metaObject
+                && element.metaObject.targetType === 'place';
+        });
+    }
+
     startRun () {
-        // this.ccpn.
+        this.ccpn.sendElement('run');
+        this.getMarking();
     }
 
     stopRun () {
-
+        this.ccpn.sendElement('stop');
+        this.getMarking();
     }
 
     terminateRun () {
-
+        this.stopRun();
     }
 
     step () {
@@ -51,6 +80,25 @@ class CCPNSimulatorPlugin {
 
     getMarking () {
         this.ccpn.sendElement('getMarking');
+    }
+
+    updateMarking (socket, netMarking) {
+        if (!netMarking.placeState || !Array.isArray(netMarking.placeState)) {
+            return;
+        }
+        for (let i = 0; i < this.places.length; i++) {
+            socket.emit('remove.label', {
+                    parentId: this.places[i].id,
+                    type: 'pt:marking',
+            });
+            if (netMarking.placeState[i].token) {
+                socket.emit('create.label', {
+                    parentId: this.places[i].id,
+                    text: netMarking.placeState[i].token,
+                    type: 'pt:marking',
+                });
+            }
+        }
     }
 
 }
